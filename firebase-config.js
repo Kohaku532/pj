09e6b9d073fed9682c35b4c8b2165d5d7d25e221
Kohaku532 <tinwinaung532@gmail.com>
2026-07-19 10:52:01 +0630

    Update and rename firebase-config.js to firebase-config.html

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCuQnYUUxtNqMV0_zYSB3frpt_rhVThvj0",
-  authDomain: "restaurant-ordering-syst-99294.firebaseapp.com",
-  projectId: "restaurant-ordering-syst-99294",
-  storageBucket: "restaurant-ordering-syst-99294.firebasestorage.app",
-  messagingSenderId: "529028598437",
-  appId: "1:529028598437:web:4e375f3383c1a691cb5b4c"
-};
