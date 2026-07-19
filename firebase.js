f9e2f22d24b501fac6dee8eb309d38c2a33b2f06
Kohaku532 <tinwinaung532@gmail.com>
2026-07-19 12:55:15 +0630

    Refactor Firebase initialization and config
    Updated Firebase imports and configuration details.

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuQnYUUxtNqMV0_zYSB3frpt_rhVThvj0",
+  authDomain: "restaurant-ordering-syst-99294.firebaseapp.com",
+  projectId: "restaurant-ordering-syst-99294",
+  storageBucket: "restaurant-ordering-syst-99294.firebasestorage.app",
+  messagingSenderId: "529028598437",
+  appId: "1:529028598437:web:4e375f3383c1a691cb5b4c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
